41d87ce10b275be6d239c5f7332d5e3f36258d9f
Доль Татьяна Васильевна <t.v.dol@yandex.ru>
2021-12-09 11:14:46 +0300

    Революция или эволюция? (часть 2)

--- src/view/menu-statistic-and-filter-view.js
+++ src/view/menu-statistic-and-filter-view.js
@@ -9,10 +9,10 @@ const createMenuStatisticAndFilterTemplate = (filters) => {
 
   return `<nav class="main-navigation">
   <div class="main-navigation__items">
-    <a href="#all" class="main-navigation__item">All movies</a>
-    <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
-    <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${history}</span></a>
-    <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
+    <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
+    <a href="#watchlist" class="main-navigation__item ">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
+    <a href="#history" class="main-navigation__item ">History <span class="main-navigation__item-count">${history}</span></a>
+    <a href="#favorites" class="main-navigation__item ">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
   </div>
   <a href="#stats" class="main-navigation__additional main-navigation__additional--active">Stats</a>
 </nav>`;
